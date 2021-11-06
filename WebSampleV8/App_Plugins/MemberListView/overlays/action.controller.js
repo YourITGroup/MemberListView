2f299d7b90aa18636a873b241ea4cef1ad5f1c7a
Robert Foster <robert@youritgroup.com.au>
2021-11-07 09:27:40 +1100

    removed stale app_plugins files

--- WebSampleV8/App_Plugins/MemberListView/overlays/action.controller.js
+++
@@ -1,9 +0,0 @@
-﻿function actionController($scope, localizationService) {
-    "use strict";
-
-    localizationService.localize($scope.model.actionKey).then(function (value) {
-        $scope.question = value
-    });
-}
-
-angular.module("umbraco").controller("MemberListView.Overlays.ActionController", actionController);
